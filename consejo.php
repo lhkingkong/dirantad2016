6afffd0d794426db928ee7871f919e5f7b0d8ffe
lhkingkong <lhkingkong@gmail.com>
2016-03-13 11:39:03 -0600

    Consejo in new style

--- consejo.php
+++ consejo.php
@@ -1,145 +1,167 @@
 <?php include ("inc/validar.php");
 $currentA='antad'; ?>
-<!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!-->
-<html lang="en">
-<!--<![endif]-->
-<head>
+  <!DOCTYPE html>
+  <html>
 
-<!-- Basic Page Needs
-  ================================================== -->
-<meta charset="utf-8">
-<title>Directorio ANTAD 2015</title>
-<meta name="description" content="">
-<meta name="author" content="">
+  <head>
+    <title>Directorio ANTAD 2016</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta charset="utf-8">
+    <meta name="author" content="Admin">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
-<!-- Mobile Specific Metas
-  ================================================== -->
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
 
-<!-- CSS
-  ================================================== -->
-<link rel="stylesheet" href="css/base.css">
-<link rel="stylesheet" href="css/skeleton.css">
-<link rel="stylesheet" href="css/layout.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-responsive.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/vertical-rhythm.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/btouleau.css">
+  </head>
 
-<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
+  <body class="appear-animate">
 
-<!-- Favicons
-	================================================== -->
-<link rel="shortcut icon" href="images/faviconAntad.ico">
-<link rel="apple-touch-icon" href="images/faviconAntad57.png">
-<link rel="apple-touch-icon" sizes="72x72" href="images/faviconAntad72.png">
-<link rel="apple-touch-icon" sizes="114x114" href="images/faviconAntad114.png">
-</head>
-<body class="ninesixcero">
-<?php include "header.php" ?>
-<?php include "mainmenu.php" ?>
-
-<!-- Primary Page Layout
-	================================================== --> 
-
-<!-- Delete everything in this .container and get started on your own site! -->
-<div class="fullscreen">
-  <div class="container contenidoA">
-    <div class="fullcontainer">
-      <div class="content_block leftTitle">
-        <p class="title">El Consejo Directivo</p>
-      </div>
+    <!-- Page Loader -->
+    <div class="page-loader">
+      <div class="loader">Loading...</div>
     </div>
-    <div class="clear"></div>
-    <div class="sixteen columns">
-		<div class="titulomovil">    
-			<p><titulos>El Consejo Directivo</titulos></p>	
-		</div>	
-		<p style="text-indent:20px;">El consejo directivo está constituído por los asociados fundadores, asociados estratégicos y asociados activos</p>
+    <!-- End Page Loader -->
 
-      <div class="boxA">
-		<h3>CONSEJO DIRECTIVO ANTAD 2015</h3>
-        <div class="list_block">
-          <ul>
-            <li><span class="text_higlight">PRESIDENTE EJECUTIVO: </span>LIC. VICENTE YÁÑEZ SOLLOA</li>
-            <li><span class="text_higlight">SECRETARIO: </span>SEARS OPERADORA MÉXICO, S.A. DE C.V.</li>
-            <li><span class="text_higlight">TESORERO: </span>TIENDAS SORIANA, S.A. DE C.V.</li>
-          </ul>
-        </div>      
-      </div>
-      
-    <div class="clear"></div>
-    <div class="sixteen columns centered">
+    <!-- Page Wrap -->
+    <div class="page" id="top">
 
-    <div class="one-third column floatLeft" style="margin:0">
-      <div class="boxA">
-        <h3>Asociados Fundadores</h3>
-        <div class="list_block">
-          <ul>
-            <li>7-ELEVEN MÉXICO, S.A. DE C.V.</li>
-            <li>ALMACENES ZARAGOZA, S.A. DE C.V.</li>
-            <li>CASA LEY, S.A. DE C.V.</li>
-            <li>EL PUERTO DE LIVERPOOL, S.A.B. DE C.V.</li>
-            <li>GRUPO COMERCIAL CONTROL, S.A. DE C.V.</li>
-            <li>GRUPO GIGANTE, S.A. DE C.V.</li>
-            <li>OPERADORA FUTURAMA, S.A. DE C.V.</li>
-            <li>TIENDAS COMERCIAL MEXICANA, S.A. DE C.V.</li>
-            <li>TIENDAS CHEDRAUI, S.A. DE C.V.</li>
-            <li>TIENDAS SORIANA, S.A. DE C.V.</li>
-          </ul>
-        </div>
-      </div>
+      <?php include "menu.php" ?>
+
+
+        <!-- Head Section -->
+        <section class="small-section bg-dark-alfa-30 parallax-2" data-background="images/background-red.png">
+          <div class="relative container align-left">
+
+            <div class="row">
+
+              <div class="col-md-12">
+                <h1 class="font-alt mb-20 mb-xs-0">Directorio ANTAD 2016</h1>
+                <h1 class="font-alt" align="center">El Consejo Directivo</h1>
+              </div>
+
+
+            </div>
+
+          </div>
+        </section>
+        <!-- End Head Section -->
+
+        <section class="page-section">
+          <div class="container relative">
+            <div class="col-md-12">
+              <p>El consejo directivo está constituído por los asociados fundadores, asociados estratégicos y asociados activos</p>
+
+              <div class="boxA">
+                <h3 class="text_higlight">CONSEJO DIRECTIVO ANTAD 2015</h3>
+                <div class="list_block">
+                  <ul>
+                    <li><span class="text_higlight">PRESIDENTE EJECUTIVO: </span>LIC. VICENTE YÁÑEZ SOLLOA</li>
+                    <li><span class="text_higlight">SECRETARIO: </span>SEARS OPERADORA MÉXICO, S.A. DE C.V.</li>
+                    <li><span class="text_higlight">TESORERO: </span>TIENDAS SORIANA, S.A. DE C.V.</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+
+              <div class="col-md-4">
+                <h3 class="text_higlight">Asociados Fundadores</h3>
+                <div class="list_block">
+                  <ul>
+                    <li>7-ELEVEN MÉXICO, S.A. DE C.V.</li>
+                    <li>ALMACENES ZARAGOZA, S.A. DE C.V.</li>
+                    <li>CASA LEY, S.A. DE C.V.</li>
+                    <li>EL PUERTO DE LIVERPOOL, S.A.B. DE C.V.</li>
+                    <li>GRUPO COMERCIAL CONTROL, S.A. DE C.V.</li>
+                    <li>GRUPO GIGANTE, S.A. DE C.V.</li>
+                    <li>OPERADORA FUTURAMA, S.A. DE C.V.</li>
+                    <li>TIENDAS COMERCIAL MEXICANA, S.A. DE C.V.</li>
+                    <li>TIENDAS CHEDRAUI, S.A. DE C.V.</li>
+                    <li>TIENDAS SORIANA, S.A. DE C.V.</li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <h3 class="text_higlight">Asociados Activos</h3>
+                <div class="list_block">
+                  <ul>
+                    <li>CIA COMERCIAL CIMACO, S.A. DE C.V.</li>
+                    <li>EL NUEVO MUNDO, S.A. DE C.V.</li>
+                    <li>HEMSA, S.A. DE C.V.</li>
+                    <li>OPERADORA DE CIUDAD JUÁREZ, S.A. DE C.V.</li>
+                    <li>OPERADORA MERCO, S.A. DE C.V.</li>
+                    <li>SEARS OPERADORA MÉXICO, S.A. DE C.V.</li>
+                    <li>SUPER SAN FRANCISCO DE ASIS, S.A. DE C.V.</li>
+                    <li>SUPERMERCADOS INTERNACIONALES HEB, S.A. DE C.V.</li>
+                    <li>TIENDAS DE AUTOSERVICIO DEL NORTE, S.A. DE C.V.</li>
+                    <li>TIENDAS GARCÉS, S.A. DE C.V.</li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <h3 class="text_higlight">Asociados Estratégicos</h3>
+                <div class="list_block">
+                  <ul>
+                    <li>CADENA COMERCIAL OXXO, S.A. DE C.V.</li>
+                    <li>COPPEL, S.A. DE C.V.</li>
+                    <li>EL PALACIO DE HIERRO, S.A. DE C.V.</li>
+                    <li>HOME DEPOT MÉXICO, S DE R.L. DE C.V.</li>
+                    <li>WAL-MART DE MÉXICO, S.A.B. DE C.V.</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <!-- container -->
+        </section>
     </div>
 
-    <div class="one-third column floatLeft" style="margin:0">
-      <div class="boxA">
-        <h3>Asociados Activos</h3>
-        <div class="list_block">
-          <ul>
-            <li>CIA COMERCIAL CIMACO, S.A. DE C.V.</li>
-            <li>EL NUEVO MUNDO,  S.A. DE C.V.</li>
-            <li>HEMSA, S.A. DE C.V.</li>
-            <li>OPERADORA DE CIUDAD JUÁREZ, S.A. DE C.V.</li>
-            <li>OPERADORA MERCO, S.A. DE C.V.</li>
-            <li>SEARS OPERADORA MÉXICO, S.A. DE C.V.</li>
-            <li>SUPER SAN FRANCISCO DE ASIS, S.A. DE C.V.</li>
-            <li>SUPERMERCADOS INTERNACIONALES HEB, S.A. DE C.V.</li>
-            <li>TIENDAS DE AUTOSERVICIO DEL NORTE, S.A. DE C.V.</li>
-            <li>TIENDAS GARCÉS, S.A. DE C.V.</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-        
-    <div class="one-third column floatLeft" style="margin:0">
-      <div class="boxA">
-        <h3>Asociados Estratégicos</h3>
-        <div class="list_block">
-          <ul>
-            <li>CADENA COMERCIAL OXXO, S.A. DE C.V.</li>
-            <li>COPPEL, S.A. DE C.V.</li>
-            <li>EL PALACIO DE HIERRO, S.A. DE C.V.</li>
-            <li>HOME DEPOT MÉXICO, S DE R.L. DE C.V.</li>
-            <li>WAL-MART DE MÉXICO, S.A.B. DE C.V.</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-       </div>
- 
+    <!-- JS -->
+    <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>        
+    <script type="text/javascript" src="js/SmoothScroll.js"></script>
+    <script type="text/javascript" src="js/jquery.scrollTo.min.js"></script>
+    <script type="text/javascript" src="js/jquery.localScroll.min.js"></script>
+    <script type="text/javascript" src="js/jquery.viewport.mini.js"></script>
+    <script type="text/javascript" src="js/jquery.countTo.js"></script>
+    <script type="text/javascript" src="js/jquery.appear.js"></script>
+    <script type="text/javascript" src="js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="js/jquery.fitvids.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
+    <script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+    <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
+    <script type="text/javascript" src="js/gmap3.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
+    <script type="text/javascript" src="js/jquery.simple-text-rotator.min.js"></script>
+    <script type="text/javascript" src="js/all.js"></script>
+    <script type="text/javascript" src="js/contact-form.js"></script>
+    <script type="text/javascript" src="js/jquery.ajaxchimp.min.js"></script>        
+    <!--[if lt IE 10]><script type="text/javascript" src="js/placeholder.js"></script><![endif]-->
+  </body>
 
-    </div> 
-
-  </div>
-  <!-- container --> 
-</div>
-</div>
-
-<!-- End Document
-================================================== -->
-
-<?php include "footer.php" ?>
-</body>
-</html>
+  </html>
